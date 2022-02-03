c3f4b16bdd9688ede578e7e8c6f5e5fd1d0ca5c3
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-02-03 16:59:13 +0100

    Change name on transformation-matrices
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A3/python/task4.py
+++ Assignments/A3/python/task4.py
@@ -24,7 +24,7 @@ p_2 = com.translate(0, P, 0) @ P_0
 p_3 = com.translate(P, P, 0) @ P_0
 
 # Selcting between task 4.3 and task4.7
-is_task_4_7 = False
+is_task_4_7 = True
 
 # Angles
 phi = 0
@@ -33,24 +33,24 @@ psi = 11.6
 
 # Base
 B = 1/2 * P
-T_platform_to_base = com.translate(B, B, 0) @ com.rotate_z(psi)
-T_base_to_platform = np.linalg.inv(T_platform_to_base) # com.rotate_z(-psi) @ com.translate(-B, -B, 0)
+T_base_to_platform = com.translate(B, B, 0) @ com.rotate_z(psi)
+#T_base_to_platform = np.linalg.inv(T_base_to_platform) # com.rotate_z(-psi) @ com.translate(-B, -B, 0)
 
 # Hinge
 H = 0.325 # Height from base to hinge in z: [m]
-T_base_to_hinge = com.translate(0, 0, H) @ com.rotate_y(theta)
-T_hinge_to_base = np.linalg.inv(T_base_to_hinge) # com.rotate_y(-theta) @ com.translate(0, 0, -H)
+T_hinge_to_base = com.translate(0, 0, H) @ com.rotate_y(theta)
+#T_hinge_to_base = np.linalg.inv(T_hinge_to_base) # com.rotate_y(-theta) @ com.translate(0, 0, -H)
 
 # Arm
 A = -0.05  # Distance from hinge to arm in z: [m]
-T_hinge_to_arm = com.translate(0, 0, A)
-T_arm_to_hinge = np.linalg.inv(T_hinge_to_arm) # com.translate(0, 0, -A)
+T_arm_to_hinge = com.translate(0, 0, A)
+#T_arm_to_hinge = np.linalg.inv(T_hinge_to_arm) # com.translate(0, 0, -A)
 
 # Rotor
 Rx = 0.65 # Distance from arm to rotor in x: [m]
 Rz = -0.03# Distance from arm to rotor in z: [m]
-T_arm_to_rotor = com.translate(Rx, 0, Rz) @ com.rotate_x(phi)
-T_rotor_to_arm = np.linalg.inv(T_arm_to_rotor) # com.rotate_x(-phi) @ com.translate(-Rx, 0, -Rz)
+T_rotor_to_arm = com.translate(Rx, 0, Rz) @ com.rotate_x(phi)
+#T_rotor_to_arm = np.linalg.inv(T_arm_to_rotor) # com.rotate_x(-phi) @ com.translate(-Rx, 0, -Rz)
 
 # Interesting points to map
 points = [p_0, p_1, p_2, p_3]
@@ -60,33 +60,33 @@ points = [p_0, p_1, p_2, p_3]
 # The last four are given in rotor frame
 
 if is_task_4_7:
-  T_platform_to_arm = T_platform_to_base @ T_base_to_hinge @ T_hinge_to_arm
+  # T_platform_to_arm = np.linalg.inv(T_base_to_platform @ T_hinge_to_base @ T_hinge_to_arm)
   T_arm_to_platform = T_base_to_platform @ T_hinge_to_base @ T_arm_to_hinge
 
-  a_0 = T_platform_to_arm @ heli_points[0]
-  a_1 = T_platform_to_arm @ heli_points[1]
-  a_2 = T_platform_to_arm @ heli_points[2]
+  a_0 = T_arm_to_platform @ heli_points[0]
+  a_1 = T_arm_to_platform @ heli_points[1]
+  a_2 = T_arm_to_platform @ heli_points[2]
 
-  T_p_r = T_platform_to_arm @ T_arm_to_rotor 
-  r_0 = T_p_r @ heli_points[3]
-  r_1 = T_p_r @ heli_points[4]
-  r_2 = T_p_r @ heli_points[5]
-  r_3 = T_p_r @ heli_points[6]
+  T_rotor_to_platform = T_arm_to_platform @ T_rotor_to_arm 
+  r_0 = T_rotor_to_platform @ heli_points[3]
+  r_1 = T_rotor_to_platform @ heli_points[4]
+  r_2 = T_rotor_to_platform @ heli_points[5]
+  r_3 = T_rotor_to_platform @ heli_points[6]
 
   points = points + [a_0, a_1, a_2, r_0, r_1, r_2, r_3]
 
   # Draw the frames
   com.draw_frame(K, T_platform_to_camera, scale=0.05)
-  com.draw_frame(K, T_platform_to_camera @ T_platform_to_base, scale=0.05)
-  com.draw_frame(K, T_platform_to_camera @ T_platform_to_base @ T_base_to_hinge, scale=0.05)
-  com.draw_frame(K, T_platform_to_camera @ T_platform_to_base @ T_base_to_hinge @ T_hinge_to_arm, scale=0.05)
-  com.draw_frame(K, T_platform_to_camera @ T_platform_to_base @ T_base_to_hinge @ T_hinge_to_arm @ T_arm_to_rotor, scale=0.05)
+  com.draw_frame(K, T_platform_to_camera @ T_base_to_platform, scale=0.05)
+  com.draw_frame(K, T_platform_to_camera @ T_base_to_platform @ T_hinge_to_base, scale=0.05)
+  com.draw_frame(K, T_platform_to_camera @ T_base_to_platform @ T_hinge_to_base @ T_arm_to_hinge, scale=0.05)
+  com.draw_frame(K, T_platform_to_camera @ T_base_to_platform @ T_hinge_to_base @ T_arm_to_hinge @ T_rotor_to_arm, scale=0.05)
 
 
-  # base = T_platform_to_base @ P_0
-  # hinge = T_platform_to_base @ T_base_to_hinge @ base
-  # arm = T_platform_to_base @ T_base_to_hinge @ T_hinge_to_arm @ hinge # Almost like the arm does not take the hinge orientation into account
-  # rotor = T_platform_to_base @ T_base_to_hinge @ T_hinge_to_arm @ T_arm_to_rotor @ arm 
+  # base = T_base_to_platform @ P_0
+  # hinge = T_base_to_platform @ T_hinge_to_base @ base
+  # arm = T_base_to_platform @ T_hinge_to_base @ T_hinge_to_arm @ hinge # Almost like the arm does not take the hinge orientation into account
+  # rotor = T_base_to_platform @ T_hinge_to_base @ T_hinge_to_arm @ T_arm_to_rotor @ arm 
   # points = points + [base, hinge, arm, rotor]
 
 # Transform the points into pixel-coordinates
@@ -95,17 +95,5 @@ X_c = T_platform_to_camera @ X_p
 
 u,v = com.project(K, X_c)
 plt.scatter(u, v, c='yellow', marker='.', s=100)
-
-# X_arm = heli_points.T[:,:3]
-# X_rotors = heli_points.T[:,3:]
-
-# T_c_a = T_platform_to_camera @ T_base_to_platform(11.6)@T_hinge_to_base(28.9)@T_arm_to_hinge()
-# T_rotors_to_camera = T_arm_to_camera@T_rotors_to_arm(0)
-
-# u,v = project(K, T_arm_to_camera@X_arm)
-# plt.scatter(u, v, c='yellow', marker='.', s=100)
-
-# u,v = project(K, T_rotors_to_camera@X_rotors)
-# plt.scatter(u, v, c='yellow', marker='.', s=100)
 
 plt.show()
