e4ebe4a988ed030d716940b8554c5688567d5e82
afanebust <afanebust@widenorth.com>
2022-02-27 12:57:15 +0100

    tried to do shit, failed

--- Projects/Midterm/python/part3.py
+++ Projects/Midterm/python/part3.py
@@ -1,8 +1,10 @@
-import os 
+import os
 import sys
+from turtle import shape
 
 import matplotlib.pyplot as plt
 import numpy as np
+from prometheus_client import Counter
 import plot_all
 import common as com
 
@@ -10,6 +12,8 @@ from scipy.optimize import least_squares, minimize
 from quanser import Quanser
 
 from numpy import character, ndarray
+
+counter = 0
 
 all_detections = np.loadtxt(os.path.join(sys.path[0], '../data/data/detections.txt'))
 quanser = Quanser()
@@ -35,44 +39,60 @@ def residual_function(
       T_p_to_c    : ndarray
     )->ndarray:
     # Extract the values from the optimization algorithm
+
+    r = np.zeros((2,7)) # residual function stats at 0
+
     marks = x[:21].reshape(3, -1)
 
-    l1 = x[21]
-    l2 = x[22]
-    l3 = x[23]
-    l4 = x[24]
-    l5 = x[25]
+    l1 = x[0]
+    l2 = x[1]
+    l3 = x[2]
+    l4 = x[3]
+    l5 = x[4]
+   
+    # x[5:25] = ?
 
-    # Only using the latest value for optimization
-    roll = x[-3]
-    pitch = x[-2]
-    yaw = x[-1]
+    for i in range(351):
 
-    # Compute the helicopter coordinate frames given the estimated states
-    base_to_platform = com.translate(l1 / 2, l1 / 2, 0.00) @ com.rotate_z(yaw)
-    hinge_to_base    = com.translate(0.00, 0.00,  l2) @ com.rotate_y(pitch)
-    arm_to_hinge     = com.translate(0.00, 0.00, l3)
-    rotors_to_arm    = com.translate(l4, 0.00, l5) @ com.rotate_x(roll)
-    base_to_camera   = T_p_to_c @ base_to_platform
-    hinge_to_camera  = base_to_camera @ hinge_to_base
-    arm_to_camera    = hinge_to_camera @ arm_to_hinge
-    rotors_to_camera = arm_to_camera @ rotors_to_arm
+      roll = x[26 + 3*i]
+      pitch = x[27 + 3*i]
+      yaw = x[28 +3*i]
 
-    # Compute the predicted image location of the marks
-    # The first three marks corresponds to the arm, while the last four corresponds to the rotor
-    # Must also have it such that the marks have dimension of 4x21
-    arm_marks = np.vstack((marks[:,:3], np.ones(marks[:,:3].shape[1])))
-    rotor_marks = np.vstack((marks[:,3:], np.ones(marks[:,3:].shape[1])))
+      # Compute the helicopter coordinate frames given the estimated states
+      base_to_platform = com.translate(l1 / 2, l1 / 2, 0.00) @ com.rotate_z(yaw)
+      hinge_to_base    = com.translate(0.00, 0.00,  l2) @ com.rotate_y(pitch)
+      arm_to_hinge     = com.translate(0.00, 0.00, l3)
+      rotors_to_arm    = com.translate(l4, 0.00, l5) @ com.rotate_x(roll)
+      base_to_camera   = T_p_to_c @ base_to_platform
+      hinge_to_camera  = base_to_camera @ hinge_to_base
+      arm_to_camera    = hinge_to_camera @ arm_to_hinge
+      rotors_to_camera = arm_to_camera @ rotors_to_arm
+  
+      # Compute the predicted image location of the marks
+      # The first three marks corresponds to the arm, while the last four corresponds to the rotor
+      # Must also have it such that the marks have dimension of 4x21
+      arm_marks = np.vstack((marks[:,:3], np.ones(marks[:,:3].shape[1])))
+      rotor_marks = np.vstack((marks[:,3:], np.ones(marks[:,3:].shape[1])))
 
-    p1 = arm_to_camera @ arm_marks
-    p2 = rotors_to_camera @ rotor_marks
-    uv_hat = com.project(K, np.hstack([p1, p2]))
+      p1 = arm_to_camera @ arm_marks
+      p2 = rotors_to_camera @ rotor_marks
+      uv_hat = com.project(K, np.hstack([p1, p2]))
 
-    # The residual-function r could only maintain the values for the optimized coordinates
-    # This means that it could only
+      # The residual-function r could only maintain the values for the optimized coordinates
+      # This means that it could only
+      w = weights[i, :]
+      u_diff = uv_hat - uv[i:i+1,:]
 
-    r = (uv_hat - uv) * weights
+      update_step = u_diff * w
+
+      r = r + update_step
     r = np.hstack([r[0], r[1]])
+
+    global counter 
+    counter += 1
+    if (counter % 100) == 0:
+      print(counter)
+
     return r
     # return np.zeros((1, 26))
 
@@ -106,9 +126,17 @@ K = np.loadtxt(os.path.join(sys.path[0], '../data/data/K.txt'))
 heli_points = np.loadtxt(os.path.join(sys.path[0], '../data/data/heli_points.txt')).T
 T_p_to_c = np.loadtxt(os.path.join(sys.path[0], '../data/data/platform_to_camera.txt'))
 
+
+
+N = 351
+Kin = 26
+M = 7
+
 method = 'A'
 if method == 'A':
-  x = np.zeros(26)
+  x = np.zeros((Kin+3*N))
+  x[:5] = np.array([0.1145, 0.325, -0.05, 0.65, -0.03]) 
+
 elif method == 'B':
   x = np.zeros(33)
 elif method == 'C':
@@ -116,21 +144,28 @@ elif method == 'C':
 else:
   raise "Invalid option" 
 
+
 all_r = []
 all_x = []
-for i in range(len(all_detections)):
-  weights = all_detections[i, ::3]
-  uv = np.vstack((all_detections[i, 1::3], all_detections[i, 2::3]))
+#for i in range(len(all_detections)):
+weights = all_detections[:, ::3]
+uv = np.vstack((all_detections[:, 1::3], all_detections[:, 2::3]))
 
-  # resfun = lambda p : quanser.residuals(uv, weights, p[0], p[1], p[2])
-  resfun = lambda x : residual_function(x, uv, weights, K, heli_points, T_p_to_c, method)
+sparsity_block = np.ones(( 2*M,3 ))
+state_sparsity =  np.kron(np.eye(N), sparsity_block)
+
+jac_sparsity = np.hstack( [ np.ones((2*M*N, Kin)), state_sparsity ] )
+
+resfun = lambda x : residual_function(x, uv, weights, K, heli_points, T_p_to_c, method)
 
   # But I cannot see how one should make this into a batch-optimization problem...
-  x = least_squares(resfun, x0=x).x
+x = least_squares(resfun, x0=x, jac_sparsity=jac_sparsity).x
   # x = minimize(resfun, x0=x).x
 
-  all_r.append(resfun(x))
-  all_x.append(x)
+  #print(resfun(x))
+
+all_r.append(resfun(x))
+all_x.append(x)
 
 all_p = np.array(all_x)
 all_r = np.array(all_r)
@@ -138,3 +173,14 @@ all_r = np.array(all_r)
 plot_all.plot_all(all_p, all_r, all_detections, subtract_initial_offset=True)
 # plt.savefig('out_part1b.png')
 plt.show()
+
+"""
+K??????????
+How can you calculated resedual function for K?
+
+What should be optimized? roll, pitch, yaw or marker positions?
+
+Task 2.3: How are we supposed to show difference, not clear what the task asks
+Task 2.3: How big difference is expected? 
+
+"""
