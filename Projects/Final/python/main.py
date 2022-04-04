6a5c1958c2019382e899f24fe74007d784cfe0b1
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-04-04 09:31:23 +0200

    Added some functions
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ Projects/Final/python/main.py
@@ -0,0 +1,11 @@
+import calibrate_camera
+import show_calibration_results
+
+def task_1_1():
+  calibrate_camera.calibrate()
+  show_calibration_results.show_results()
+
+if __name__ == '__main__':
+  # Task 1.1
+  task_1_1()
+  
