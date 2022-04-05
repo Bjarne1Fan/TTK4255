cc72dc570bd65ad14acede6c3a227388deb77d75
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-04-05 15:31:49 +0200

    Add estimation of essential matrix
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Projects/Final/python/estimate_E.py
+++ Projects/Final/python/estimate_E.py
@@ -41,10 +41,7 @@ def ransac(
       num_trials          : int
     ) -> np.ndarray:
 
-  # Tip: The following snippet extracts a random subset of 8
-  # correspondences (w/o replacement) and estimates E using them.
-  #   sample = np.random.choice(xy1.shape[1], size=8, replace=False)
-  #   E = estimate_E(xy1[:,sample], xy2[:,sample])
+  K_inv = np.linalg.inv(K)
 
   # Defining memory
   E = np.eye(3)
@@ -59,22 +56,25 @@ def ransac(
     estimated_E = eight_point_algorithm(xy1[:,sample], xy2[:,sample])
 
     # Calculate the fundamental matrix and the residuals
-    F = F_from_E.F_from_E(E=E, K=K)
+    F = K_inv.T @ E @ K_inv #F_from_E.F_from_E(E=E, K=K)
     residuals = epipolar_distance.epipolar_distance(F=F, uv1=uv1, uv2=uv2)
-    avg_residuals = 1/2.0 * (residuals[0,:] + residuals[1,:])
-    abs_avg_residuals = np.abs(avg_residuals)
+    inliers = np.abs(residuals) < distance_threshold
+    num_inliers = np.sum(inliers)
+    #avg_residuals = 1/2.0 * (residuals[0,:] + residuals[1,:])
+    #abs_avg_residuals = np.abs(avg_residuals)
 
     # Check number of points being inliers
-    num_inliers = np.sum(abs_avg_residuals <= distance_threshold)
+    # num_inliers = np.sum(abs_avg_residuals <= distance_threshold)
 
     if num_inliers >= max_num_inliers:
-      print("Better sample found at iteration number: {}".format(i))
+      # print("Better sample found at iteration number: {}".format(i))
       max_num_inliers = num_inliers
-      inlier_set = np.where(abs_avg_residuals <= distance_threshold)[0]
+      inlier_set = inliers#np.where(abs_avg_residuals <= distance_threshold)[0]
       E = estimated_E
 
     if i % 100 == 0 and i > 0:
       print(i)
+  inlier_set = np.where(inlier_set == True)[0]
 
   return E, inlier_set 
 
