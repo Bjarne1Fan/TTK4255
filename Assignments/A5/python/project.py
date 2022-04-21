e6cc8c1b37a75c662635615716a66b8205b604ab
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-04-21 17:25:56 +0200

    Solve bug
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A5/python/project.py
+++ Assignments/A5/python/project.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def project(
-      arr   : np.ndarray, 
-      K_inv : np.ndarray
+      arr : np.ndarray, 
+      K   : np.ndarray
     ) -> np.ndarray:
 
-  arr_tilde = K_inv @ arr
+  arr_tilde = K @ arr
   return arr_tilde[:2,:] / arr_tilde[2,:]
